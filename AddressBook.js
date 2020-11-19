98b01648b04a942e2f17e60dcd03536c09bc3d4e
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-11-19 16:56:19 +0530

    [Kartikeya] UC added to add contact details in address book array

--- AddressBook.js
+++ AddressBook.js
@@ -147,3 +147,108 @@
     let addressBookContacts = new AddressBookContacts("Kumar", "Kartikeya", "Techman City", "Agra", "Uttar Pradesh", "281006", "7206183244", "kumar.kartikeya@capgemini.com");
     console.log("UC 2------"+addressBookContacts.toString());
 }
+
+// UC 3
+// Adding contacts in address book array
+{
+    class AddressBookContacts {
+        
+        // constructor
+        constructor(...params) {
+            this.firstName = params[0];
+            this.lastName = params[1];
+            this.address = params[2];
+            this.city = params[3];
+            this.state = params[4];
+            this.zip = params[5];
+            this.phoneNo = params[6];
+            this.email = params[7];
+        }
+
+        get firstName(){ return this._firstName; }
+        set firstName(firstName) 
+        {
+            let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+            if (nameRegex.test(firstName))
+                this._firstName = firstName;
+            else throw 'First name is incorrect!';
+        }
+
+        get lastName() { return this._lastName; }
+        set lastName(lastName) 
+        {
+            let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+            if (nameRegex.test(lastName))
+                this._lastName = lastName;
+            else throw 'Last name is incorrect!';
+        }
+    
+        get address(){ return this._address; }
+        set address(address)
+        {
+            let addressRegex= RegExp('^[A-Z0-9a-z ]{4,}$');
+            if(addressRegex.test(address))
+                this._address= address;
+            else throw "Incorrect Address";
+        }
+    
+        get city(){ return this._city; }
+        set city(city)
+        {
+            let cityRegex= RegExp('^[A-Z0-9a-z ]{4,}$');
+            if(cityRegex.test(city))
+                this._city= city;
+            else throw "Incorrect city";
+        }
+    
+        get state(){ return this._state; }
+        set state(state)
+        {
+            let stateRegex= RegExp('^[A-Z0-9a-z ]{4,}$');
+            if(stateRegex.test(state))
+                this._state= state;
+            else throw "Incorrect state";
+        }
+
+        get zip(){ return this._zip; }
+        set zip(zip)
+        {
+            let zipRegex= RegExp('^[0-9]{1}[a-zA-Z0-9]{2}[ ]*[a-zA-Z0-9]{2}[0-9]{1}$');
+            if(zipRegex.test(zip))
+                this._zip= zip;
+            else throw "Incorrect zip";
+        }
+
+        get phoneNo(){ return this._phoneNo; }
+        set phoneNo(phoneNo)
+        {
+            let phoneNoRegex= RegExp('^[1-9]{1}[0-9]{9}$');
+            if(phoneNoRegex.test(phoneNo))
+                this._phoneNo= phoneNo;
+            else throw "Incorrect phone number";
+        }
+
+        get email(){ return this._email; }
+        set email(email)
+        {
+            let emailRegex= RegExp('[a-zA-Z0-9]+([+-_.][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})*$');
+            if(emailRegex.test(email))
+                this._email= email;
+            else throw "Incorrect email";
+        }
+    
+        // method 
+        toString() 
+        {
+            return "FirstName=" + this.firstName + ", LastName=" + this.lastName + ", Address=" + this.address + ", City=" + this.city + ", State=" + this.state + ", Zip=" + this.zip + ", Phone Number=" + this.phoneNo + ", Email=" + this.email;
+        }
+    }
+    let addressBookArr = new Array();
+    let addressBookContacts = new AddressBookContacts("Kumar", "Kartikeya", "Techman City", "Agra", "Uttar Pradesh", "281006", "7206183244", "kumar.kartikeya@capgemini.com");
+    addressBookArr.push(addressBookContacts);
+    console.log("UC 3-----");
+    for(let i=0; i<addressBookArr.length; i++)
+    {
+        console.log(addressBookArr[i]);
+    }
+}
